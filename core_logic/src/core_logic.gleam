6a0a74b96e7d82f9e2743941d826870233d72201
ZeroiJ <sujalbirwadkar19@gmail.com>
2026-01-30 19:13:16 +0530

    chore: remove gleam from project

--- core_logic/src/core_logic.gleam
+++
@@ -1,9 +0,0 @@
-import gleam/io
-
-pub fn main() {
-  io.println("Hello from core_logic!")
-}
-
-pub fn hello() -> String {
-  "Hello from Gleam Core Logic!"
-}
--- src/app/api/hello-gleam/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-
-import { hello } from '../../../../core_logic/build/dev/javascript/core_logic/core_logic.mjs';
-
-export const runtime = 'edge';
-
-export async function GET() {
-    return NextResponse.json({ message: hello() });
-}
